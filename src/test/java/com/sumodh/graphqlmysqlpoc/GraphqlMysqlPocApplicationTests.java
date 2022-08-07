9bd2222b3af05e2109764298faf041a232df06bf
Sumodh Sudhakaran <sumodh.sudhakaran@tesco.com>
2022-08-07 23:53:47 +0530

    Initial commit

---
+++ src/test/java/com/sumodh/graphqlmysqlpoc/GraphqlMysqlPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumodh.graphqlmysqlpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlMysqlPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
